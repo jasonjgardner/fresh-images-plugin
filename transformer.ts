50e6471a4979795bd97e05f52045d3883bd82b95
Jason Gardner <im@jasongardner.co>
2023-10-11 15:05:10 -0500

    feat: Rotation transformer
    - Added documentation
    - Added "rotate" transformation function

--- transformer.ts
+++ transformer.ts
@@ -4,6 +4,35 @@ import {
   Image,
 } from "https://deno.land/x/imagescript@1.2.15/mod.ts";
 
+/**
+ * Resize an image to a given width and/or height.
+ * @param img Image to resize
+ * @param req URL request containing transformation parameters
+ * @returns Resized image
+ * @example
+ * ```ts
+ * import { defineConfig } from "$fresh/server.ts";
+ * import ImagesPlugin from "fresh-images/mod.ts";
+ * import { resize } from "fresh-images/transformer.ts";
+ *
+ * export default defineConfig({
+ * plugins: [
+ *  ImagesPlugin({
+ *  publicPath: "/img",
+ * transformers: { resize },
+ * }),
+ * ],
+ * ```
+ * @example
+ * ```html
+ * <img src="/img/meow.jpg?fn=resize&w=100" alt="Resized to 100px wide" />
+ * <img src="/img/meow.jpg?fn=resize&width=100" alt="Resized to 100px wide" />
+ * <img src="/img/meow.jpg?fn=resize&h=100" alt="Resized to 100px tall" />
+ * <img src="/img/meow.jpg?fn=resize&height=100" alt="Resized to 100px tall" />
+ * <img src="/img/meow.jpg?fn=resize&w=100&h=100" alt="Resized to 100px by 100px" />
+ * <img src="/img/meow.jpg?fn=resize&width=100&height=100" alt="Resized to 100px by 100px" />
+ * ```
+ */
 export function resize(img: Image | GIF, req: Request): Image | GIF {
   const url = new URL(req.url);
 
@@ -12,17 +41,69 @@ export function resize(img: Image | GIF, req: Request): Image | GIF {
   const height = url.searchParams.get("h") ?? url.searchParams.get("height") ??
     Image.RESIZE_AUTO;
 
+  // FIXME: Height must be specified when transforming a GIF
   if (img instanceof GIF) {
     // Resize each frame and reconstruct the GIF
-    const frames: Frame[] = img.map((imgFrame) => {
-      const frame = new Frame(imgFrame.width, imgFrame.height);
+    const frames: Frame[] = [];
+
+    img.forEach((imgFrame) => {
+      const frame = new Frame(Number(width), Number(height));
       frame.bitmap = imgFrame.resize(Number(width), Number(height)).bitmap;
 
-      return frame;
+      frames.push(frame);
     });
 
     return new GIF(frames);
   }
 
   return img.resize(Number(width), Number(height));
+}
+
+/**
+ * Rotate an image by a given number of degrees.
+ * @param img Image to rotate
+ * @param req URL request containing transformation parameters
+ * @returns Rotated image
+ * @example
+ * ```ts
+ * import { defineConfig } from "$fresh/server.ts";
+ * import ImagesPlugin from "fresh-images/mod.ts";
+ * import { rotate } from "fresh-images/transformer.ts";
+ *
+ * export default defineConfig({
+ *  plugins: [
+ *   ImagesPlugin({
+ *    publicPath: "/img",
+ *   transformers: { rotate },
+ *  }),
+ * ],
+ * ```
+ * @example
+ * ```html
+ * <img src="/img/meow.jpg?fn=rotate&d=270" alt="Rotated 270 degrees" />
+ * <img src="/img/meow.jpg?fn=rotate&degrees=90" alt="Rotated 90 degrees" />
+ * <img src="/img/meow.jpg?fn=rotate&degrees=-45" alt="Rotated -45 degrees" />
+ * ```
+ */
+export function rotate(img: Image | GIF, req: Request): Image | GIF {
+  const url = new URL(req.url);
+
+  const degrees = url.searchParams.get("d") ?? url.searchParams.get("degrees");
+
+  if (img instanceof GIF) {
+    // Rotate each frame and reconstruct the GIF
+    const frames: Frame[] = [];
+
+    img.forEach((imgFrame) => {
+      imgFrame.rotate(Number(degrees));
+      const frame = new Frame(imgFrame.width, imgFrame.height);
+      frame.bitmap = imgFrame.bitmap;
+
+      frames.push(frame);
+    });
+
+    return new GIF(frames);
+  }
+
+  return img.rotate(Number(degrees)) as Image;
 }
