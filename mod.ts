4c2f175699db505679b9ae64d826d48b0013ab1a
Jason Gardner <im@jasongardner.co>
2023-10-11 15:06:21 -0500

    feat: Quality parameter
    - Added transformation chaining
    - Added image encoding quality parameter

--- mod.ts
+++ mod.ts
@@ -1,11 +1,8 @@
 import { asset, ASSET_CACHE_BUST_KEY } from "$fresh/runtime.ts";
-import type { Handler, Plugin } from "$fresh/server.ts";
-import {
-  decode,
-  GIF,
-  Image,
-} from "https://deno.land/x/imagescript@1.2.15/mod.ts";
-import { extname, join, resolve, toFileUrl } from "$std/path/mod.ts";
+import type { Plugin } from "$fresh/server.ts";
+import type { PluginRoute } from "$fresh/src/server/types.ts";
+import { decode, GIF, Image } from "imagescript/mod.ts";
+import { join, resolve, toFileUrl } from "$std/path/mod.ts";
 
 export interface ImagesPluginOptions {
   publicPath?: string;
@@ -27,6 +24,9 @@ export async function handleImageRequest<T extends string>(
 ): Promise<Response> {
   const url = new URL(req.url);
   const srcPath = url.pathname.replace(`${publicPath}/`, "");
+
+  // TODO: Add support for external image sources
+
   const resourcePath = toFileUrl(
     join(resolve(Deno.cwd(), localPath ?? "./"), srcPath),
   );
@@ -38,15 +38,19 @@ export async function handleImageRequest<T extends string>(
     const data = await res.arrayBuffer();
     const img = await transformFns.reduce(async (acc, xfn) => {
       if (xfn in transformers) {
-        const newImg = await transformers[xfn](await acc, req);
-        return newImg;
+        return await transformers[xfn](await acc, req);
       }
 
       return acc;
     }, decode(data));
 
     const isGif = img instanceof GIF;
-    const buffer = await (isGif ? img.encode(15) : (img as Image).encode(1));
+    const quality = Number(
+      url.searchParams.get("q") ?? url.searchParams.get("quality") ?? 5,
+    );
+    const buffer = await img.encode(
+      Math.min(isGif ? 30 : 3, Math.max(1, quality)),
+    );
 
     return new Response(buffer, {
       headers: {
@@ -61,38 +65,52 @@ export async function handleImageRequest<T extends string>(
   }
 }
 
-export function createRoutes<T extends string = "/images">(
-  imagesPath = "/images" as T,
-  absoluteBasePath = "./static",
-  transformers: ImagesPluginOptions["transformers"] = {},
-): [
-  { path: `${T}/[fileName]`; handler: Handler },
-] {
-  // TODO: Ensure imagePath is not a directory in the ./static folder. Otherwise there will be Fresh routing conflicts.
-
-  // TODO: Allow image transformers to create their own routes
-
-  return [
-    {
-      path: `${imagesPath}/[fileName]`,
-      handler: async (req) =>
-        await handleImageRequest(
-          transformers,
-          req,
-          imagesPath,
-          absoluteBasePath,
-        ),
-    },
-  ];
-}
-
+/**
+ * A Fresh plugin which adds image transformation routes to your static directory
+ * @property {string} publicPath The base path for the image transformation routes
+ * @property {string} realPath The absolute path to the image file directory
+ * @property {Record<string, (img: Image | GIF, req: Request) => Image | GIF>} transformers A map of image transformation functions
+ * @returns {Plugin} Images plugin
+ * @example
+ * ```ts
+ * import { defineConfig } from "$fresh/server.ts";
+ * import ImagesPlugin from "fresh-images/mod.ts";
+ * import { resize } from "fresh-images/transformer.ts";
+ *
+ * export default defineConfig({
+ *   plugins: [
+ *     ImagesPlugin({
+ *       publicPath: "/img",
+ *       transformers: { resize },
+ *     }),
+ *   ],
+ * });
+ * ```
+ */
 export default function ImagesPlugin({
   publicPath = "/images",
   realPath = "./static/image",
   transformers = {},
 }: ImagesPluginOptions): Plugin {
+  // TODO: Ensure imagePath is not a directory in the ./static folder. Otherwise there will be Fresh routing conflicts.
+
+  // TODO: Allow image transformers to create their own routes
+
+  const routes: PluginRoute[] = [
+    {
+      path: `${publicPath}/[fileName]`,
+      handler: async (req) =>
+        await handleImageRequest(
+          transformers,
+          req,
+          publicPath,
+          realPath,
+        ),
+    },
+  ];
+
   return {
     name: "fresh-images",
-    routes: createRoutes(publicPath, realPath, transformers),
+    routes,
   };
 }
